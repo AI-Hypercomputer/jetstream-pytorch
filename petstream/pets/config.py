6f93217ac4351ac551ba03bb8106c41563f798e0
Han Qi <hanq@google.com>
2024-03-05 00:56:52 +0000

    Add files to run server

---
+++ petstream/pets/config.py
@@ -0,0 +1,30 @@
+from jetstream.core.config_lib import ServerConfig
+from petstream.jet_engine import create_pytorch_engine
+
+
+def create_config(
+            devices,
+            tokenizer_path,
+            ckpt_path,
+            bf16_enable,
+            param_size,
+            context_length,
+            batch_size,
+):
+    def func(a):
+        from petstream.jet_engine import PyTorchEngine
+        return create_pytorch_engine(
+            devices=devices,
+            tokenizer_path=tokenizer_path,
+            ckpt_path=ckpt_path,
+            bf16_enable=bf16_enable,
+            param_size='tiny',
+            context_length=context_length,
+            batch_size=batch_size,
+        )
+
+
+    return ServerConfig(
+        interleaved_slices=('tpu=4', ),
+        interleaved_engine_create_fns=(func, ),
+    )
