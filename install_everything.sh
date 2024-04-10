4d38e611ade4fde67a11e421c957523d600467f3
Zijun Zhou <zzzijunzhou@gmail.com>
2024-04-10 16:41:56 -0700

    Fix unit tests CICD (#13)
    * Fix unit tests CICD
    
    * Add coverage dep
    
    * Enforce cpu jax platform

--- install_everything.sh
+++ install_everything.sh
@@ -3,7 +3,7 @@ TORCHXLA_TAG=jetstream-pytorch
 pip3 install pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 # torch cpu
 pip3 install torch --index-url https://download.pytorch.org/whl/cpu
-pip3 install tensorflow flatbuffers absl-py flax sentencepiece seqio google-cloud-storage safetensors colorama
+pip3 install tensorflow flatbuffers absl-py flax sentencepiece seqio google-cloud-storage safetensors colorama coverage
 
 mkdir -p deps
 pushd deps
