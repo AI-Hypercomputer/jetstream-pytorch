90ef0f33b8e3ef4a2008ad22a4b867ec9fd507d0
Fanhai Lu <154379058+FanhaiLu1@users.noreply.github.com>
2024-04-23 13:25:26 -0700

    Add original llama prefill and decode (#25)
    add original prefill and decode

--- tests/test_llama_e2e.py
+++ tests/test_llama_e2e.py
@@ -353,7 +353,137 @@ class LlamaE2ETest(unittest.TestCase):
                 break
         print(f"-------------------->out_tokens:{out_tokens}")
         print(f"-------------------->expected_output_tokens:{expected_output_tokens}")
-        # self.assertEqual(out_tokens ,expected_output_tokens)        
+        # self.assertEqual(out_tokens ,expected_output_tokens)   
+  
+    def test_llama_with_original_prefill_decode_32(self):
+        jax.config.update('jax_platform_name', 'cpu')
+        print(f"---------> {jax.devices()}")
+
+        torch.set_default_dtype(torch.float32)
+        env = self._make_env(bf16_enable=False)
+        model_arg = env._model_arg 
+        tokens = np.arange(10, dtype=np.int32)
+        true_length = tokens.shape[-1]
+        padded_tokens = np.pad(tokens, (0, 6))
+        padded_tokens = jnp.array(padded_tokens)
+
+        seed = 1
+        torch.manual_seed(1)
+        max_output_length = 5
+
+        file_dir = os.path.dirname(__file__)
+        tokenizer_path = os.path.join(file_dir, '../jetstream_pt/third_party/llama2/tokenizer.model')
+
+        # orginal
+        llama_original = LlamaOriginal.build(tokenizer_path, model_arg, seed)
+        model_orig = llama_original.model
+        state_dict = dict(model_orig.state_dict())
+        state_dict['freqs_cis'] = model_orig.freqs_cis
+        model_ours = model_exportable.Transformer(model_arg, env)
+        engine = PyTorchEngine(
+            pt_model=model_ours,
+            env=env
+        )
+        params = self._to_jax(state_dict)
+        slot = 0 
+        expected_output_tokens = []
+        out_tokens = []
+
+
+        prompt_tokens = [tokens]
+        decode_state_original = llama_original.prefill(prompt_tokens, max_output_length)
+
+        decode_state = engine.init_decode_state()
+        prefill_result = engine.prefill(
+            params=params, padded_tokens=padded_tokens, true_length=true_length
+        )
+        out_tokens = prefill_result.token
+        self.assertEquals(out_tokens, decode_state_original.out_tokens[0][0])
+        print(f"-------------------->: prefill step: --->")
+        print(f"-------------------->orginal out_tokens: {decode_state_original.out_tokens}")
+        print(f"-------------------->out_tokens: {out_tokens}") 
+        
+        
+        # self._diff_value(prefill_result.logits[0:10, :], torch.squeeze(decode_state_original.logits), "prefill logits")
+        decode_state = engine.insert(
+            prefill_result, decode_state, slot=slot
+        )
+      
+    
+        for i in range(0, max_output_length - 1):
+            decode_state_original = llama_original.decode(decode_state_original)
+            decode_state, result_tokens = engine.generate(params, decode_state)
+
+            # self._diff_value(decode_state.logits, decode_state_original.logits, "prefill logits")
+           
+            self.assertEquals(np.asanyarray(decode_state.tokens), np.array(decode_state_original.out_tokens))
+            print(f"-------------------->: decode step {i}: --->")
+            print(f"-------------------->orginal out_tokens: {decode_state_original.out_tokens}")
+            print(f"-------------------->out_tokens: {decode_state.tokens}")   
+            
+    def test_llama_with_original_prefill_decode(self):
+        jax.config.update('jax_platform_name', 'cpu')
+        print(f"---------> {jax.devices()}")
+
+        torch.set_default_dtype(torch.float32)
+        env = self._make_env()
+        model_arg = env._model_arg 
+        tokens = np.arange(10, dtype=np.int32)
+        true_length = tokens.shape[-1]
+        padded_tokens = np.pad(tokens, (0, 6))
+        padded_tokens = jnp.array(padded_tokens)
+
+        seed = 1
+        torch.manual_seed(1)
+        max_output_length = 5
+
+        file_dir = os.path.dirname(__file__)
+        tokenizer_path = os.path.join(file_dir, '../jetstream_pt/third_party/llama2/tokenizer.model')
+
+        # orginal
+        llama_original = LlamaOriginal.build(tokenizer_path, model_arg, seed)
+        model_orig = llama_original.model
+        state_dict = dict(model_orig.state_dict())
+        state_dict['freqs_cis'] = model_orig.freqs_cis
+        model_ours = model_exportable.Transformer(model_arg, env)
+        engine = PyTorchEngine(
+            pt_model=model_ours,
+            env=env
+        )
+        params = self._to_jax(state_dict)
+        slot = 0 
+        expected_output_tokens = []
+        out_tokens = []
+
+
+        prompt_tokens = [tokens]
+        decode_state_original = llama_original.prefill(prompt_tokens, max_output_length)
+
+        decode_state = engine.init_decode_state()
+        prefill_result = engine.prefill(
+            params=params, padded_tokens=padded_tokens, true_length=true_length
+        )
+        out_tokens = prefill_result.token
+        print(f"-------------------->: prefill step: --->")
+        print(f"-------------------->orginal out_tokens: {decode_state_original.out_tokens}")
+        print(f"-------------------->out_tokens: {out_tokens}") 
+        
+        
+        # self._diff_value(prefill_result.logits[0:10, :], torch.squeeze(decode_state_original.logits), "prefill logits")
+        decode_state = engine.insert(
+            prefill_result, decode_state, slot=slot
+        )
+      
+    
+        for i in range(0, max_output_length - 1):
+            decode_state_original = llama_original.decode(decode_state_original)
+            decode_state, result_tokens = engine.generate(params, decode_state)
+
+            # self._diff_value(decode_state.logits, decode_state_original.logits, "prefill logits")
+            print(f"-------------------->: decode step {i}: --->")
+            print(f"-------------------->orginal out_tokens: {decode_state_original.out_tokens}")
+            print(f"-------------------->out_tokens: {decode_state.tokens}")          
+             
 
 if __name__ == '__main__':
     unittest.main()
