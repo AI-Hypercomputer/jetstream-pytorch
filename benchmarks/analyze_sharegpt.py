55280546c8e604204fcfd69ae99b2f35a23271e8
FanhaiLu1 <fanhai@google.com>
2024-04-24 21:07:17 +0000

    cleanup and format benchmark code

--- benchmarks/analyze_sharegpt.py
+++ benchmarks/analyze_sharegpt.py
@@ -18,98 +18,146 @@ CUTOFF_INPUT = 1024
 CUTOFF_OUTPUT = 1024
 
 # batch size 60, ful cache, bfloat
-prefill_bucket_size_to_s = {64: 0.007696230011060834, 128: 0.011508351005613805, 256: 0.01721684739459306, 512: 0.03257157760672271, 1024: 0.08185497261583805}
+prefill_bucket_size_to_s = {
+    64: 0.007696230011060834,
+    128: 0.011508351005613805,
+    256: 0.01721684739459306,
+    512: 0.03257157760672271,
+    1024: 0.08185497261583805,
+}
 
 # batch size 96, ful cache, quantized
-prefill_bucket_size_to_s = {64: 0.006911616190336645, 128: 0.011646182998083532, 256: 0.01875854718964547, 512: 0.0334438294172287, 1024: 0.0643601292045787}
+prefill_bucket_size_to_s = {
+    64: 0.006911616190336645,
+    128: 0.011646182998083532,
+    256: 0.01875854718964547,
+    512: 0.0334438294172287,
+    1024: 0.0643601292045787,
+}
 
 # batch size 96, rolling, bfloat
-prefill_bucket_size_to_s = {64: 0.007730783987790346, 128: 0.011515899002552033, 256: 0.01780580161139369, 512: 0.03115477201063186, 1024: 0.07443338260054588}
+prefill_bucket_size_to_s = {
+    64: 0.007730783987790346,
+    128: 0.011515899002552033,
+    256: 0.01780580161139369,
+    512: 0.03115477201063186,
+    1024: 0.07443338260054588,
+}
 
-# batch size 160, rolling, quantized 
-prefill_bucket_size_to_s = {64: 0.006821704190224409, 128: 0.01175499300006777, 256: 0.018776051187887787, 512: 0.03392685519065708, 1024: 0.06476318498607725}
+# batch size 160, rolling, quantized
+prefill_bucket_size_to_s = {
+    64: 0.006821704190224409,
+    128: 0.01175499300006777,
+    256: 0.018776051187887787,
+    512: 0.03392685519065708,
+    1024: 0.06476318498607725,
+}
 
-prefill_bucket_size_to_ms = {k: p*1000 for k, p in prefill_bucket_size_to_s.items() }
+prefill_bucket_size_to_ms = {
+    k: p * 1000 for k, p in prefill_bucket_size_to_s.items()
+}
 
 # batch size 60, ful cache, bfloat
-system_time_per_decode_token_ms =26.55/60
+system_time_per_decode_token_ms = 26.55 / 60
 
 # batch size 96, ful cache, quantized
-system_time_per_decode_token_ms =26.0/96
+system_time_per_decode_token_ms = 26.0 / 96
 
 # batch size 96, rolling, bfloat
-system_time_per_decode_token_ms =28.18/96
+system_time_per_decode_token_ms = 28.18 / 96
 
-# batch size 160, rolling, quantized 
-system_time_per_decode_token_ms =30/160
+# batch size 160, rolling, quantized
+system_time_per_decode_token_ms = 30 / 160
+
 
 def do_simulation(prefill_bucket_size_to_ms, system_time_per_decode_token_ms):
 
-    def next_power_of_2(x):  
-        return 1 if x == 0 else 2**(x - 1).bit_length()
+  def next_power_of_2(x):
+    return 1 if x == 0 else 2 ** (x - 1).bit_length()
 
-    def tokens_in_input_str(s):
-        return_val =  int(1.3 * len(s.split()))
-        #print(f"{s=} -> {return_val=}")
-        return return_val
+  def tokens_in_input_str(s):
+    return_val = int(1.3 * len(s.split()))
+    # print(f"{s=} -> {return_val=}")
+    return return_val
 
-    convo_numbers = []
-    # Please update with your own data file path
-    loaded_share_gpt = json.load(open('~/data/ShareGPT_V3_unfiltered_cleaned_split.json', 'r'))
-    for example in loaded_share_gpt:
-        if len(example['conversations']) < 2:
-            continue
-        input_tokens = tokens_in_input_str(example['conversations'][0]['value'])
-        output_tokens = tokens_in_input_str(example['conversations'][1]['value'])
-        convo_numbers.append( (input_tokens, output_tokens))
+  convo_numbers = []
+  # Please update with your own data file path
+  loaded_share_gpt = json.load(
+      open("~/data/ShareGPT_V3_unfiltered_cleaned_split.json", "r")
+  )
+  for example in loaded_share_gpt:
+    if len(example["conversations"]) < 2:
+      continue
+    input_tokens = tokens_in_input_str(example["conversations"][0]["value"])
+    output_tokens = tokens_in_input_str(example["conversations"][1]["value"])
+    convo_numbers.append((input_tokens, output_tokens))
 
-    num_convos = len(convo_numbers)
-    kept_convos = [c for c in convo_numbers if c[0] <= CUTOFF_INPUT and c[1] <= CUTOFF_OUTPUT]
+  num_convos = len(convo_numbers)
+  kept_convos = [
+      c for c in convo_numbers if c[0] <= CUTOFF_INPUT and c[1] <= CUTOFF_OUTPUT
+  ]
 
-    mean_input = sum([c[0] for c in kept_convos]) / len(kept_convos)
-    mean_output = sum([c[1] for c in kept_convos]) / len(kept_convos)
+  mean_input = sum([c[0] for c in kept_convos]) / len(kept_convos)
+  mean_output = sum([c[1] for c in kept_convos]) / len(kept_convos)
 
-    print(f"Total {num_convos=} but only kept {kept_convos=}. Out of kept, {mean_input=}, {mean_output=}")
+  print(
+      f"Total {num_convos=} but only kept {kept_convos=}. Out of kept, {mean_input=}, {mean_output=}"
+  )
 
-    total_prefill_system_ms = 0
-    total_generate_system_ms = 0
+  total_prefill_system_ms = 0
+  total_generate_system_ms = 0
 
-    total_system_output_tokens = 0
-    for convo in kept_convos:
-        input_tok, output_tok = convo
-        bucket = max(128,next_power_of_2(input_tok))
-        generate_system_ms = output_tok * system_time_per_decode_token_ms 
-        prefill_system_ms = prefill_bucket_size_to_ms[bucket]
+  total_system_output_tokens = 0
+  for convo in kept_convos:
+    input_tok, output_tok = convo
+    bucket = max(128, next_power_of_2(input_tok))
+    generate_system_ms = output_tok * system_time_per_decode_token_ms
+    prefill_system_ms = prefill_bucket_size_to_ms[bucket]
 
-        print(f"{convo=} {bucket=}, {prefill_system_ms=:.2f}, {generate_system_ms=:.2f}")
+    print(
+        f"{convo=} {bucket=}, {prefill_system_ms=:.2f}, {generate_system_ms=:.2f}"
+    )
 
-        total_prefill_system_ms += prefill_system_ms
-        total_generate_system_ms += generate_system_ms
+    total_prefill_system_ms += prefill_system_ms
+    total_generate_system_ms += generate_system_ms
 
-    total_time_ms = total_prefill_system_ms + total_generate_system_ms
-    input_tokens = sum([c[0] for c in kept_convos])
+  total_time_ms = total_prefill_system_ms + total_generate_system_ms
+  input_tokens = sum([c[0] for c in kept_convos])
 
-    output_tokens = sum([c[1] for c in kept_convos])
-    print(f"Output tokens {output_tokens} in {total_time_ms/1000:.2f} seconds, for {output_tokens/(total_time_ms/1000):.2f} out tok/s")
+  output_tokens = sum([c[1] for c in kept_convos])
+  print(
+      f"Output tokens {output_tokens} in {total_time_ms/1000:.2f} seconds, for {output_tokens/(total_time_ms/1000):.2f} out tok/s"
+  )
 
-    total_prefill_sec = total_prefill_system_ms/1000
-    total_generate_sec = total_generate_system_ms/1000
+  total_prefill_sec = total_prefill_system_ms / 1000
+  total_generate_sec = total_generate_system_ms / 1000
 
-    print(f"Total time {total_time_ms/1000:.2f} seconds, split {total_prefill_sec=:.2f} seconds and {total_generate_sec=:.2f} seconds")
+  print(
+      f"Total time {total_time_ms/1000:.2f} seconds, split {total_prefill_sec=:.2f} seconds and {total_generate_sec=:.2f} seconds"
+  )
 
-    idealized_prefill_sec = 1.1 * input_tokens/1024 * prefill_bucket_size_to_ms[1024] / 1000
+  idealized_prefill_sec = (
+      1.1 * input_tokens / 1024 * prefill_bucket_size_to_ms[1024] / 1000
+  )
 
-    prefill_savings_sec = total_prefill_sec-idealized_prefill_sec
+  prefill_savings_sec = total_prefill_sec - idealized_prefill_sec
 
+  idealized_generate_sec = (
+      total_generate_sec / 2
+  )  # (Roughly save 75% on KV cache high cost on the rest)
+  generate_savings_sec = total_generate_sec - idealized_generate_sec
 
-    idealized_generate_sec = total_generate_sec/2 # (Roughly save 75% on KV cache high cost on the rest)
-    generate_savings_sec = total_generate_sec - idealized_generate_sec
+  print(
+      f"we think prefill will take {total_prefill_sec=:.2f}, we could get it to {idealized_prefill_sec=:.2f} so we'd save {prefill_savings_sec=:.2f} seconds "
+  )
+  print(
+      f"with sparsity we could go from  {total_generate_sec=:.2f}, we could get it to {idealized_generate_sec=:.2f} so we'd save {generate_savings_sec=:.2f} seconds "
+  )
 
-    print(f"we think prefill will take {total_prefill_sec=:.2f}, we could get it to {idealized_prefill_sec=:.2f} so we'd save {prefill_savings_sec=:.2f} seconds ")
-    print(f"with sparsity we could go from  {total_generate_sec=:.2f}, we could get it to {idealized_generate_sec=:.2f} so we'd save {generate_savings_sec=:.2f} seconds ")
+  idealized_overall_time = idealized_generate_sec + idealized_prefill_sec
 
-    idealized_overall_time = idealized_generate_sec + idealized_prefill_sec
-
-    print(f"Idealized out tokens {output_tokens} in {idealized_overall_time:.2f} seconds, for {output_tokens/idealized_overall_time:.2f} out tok/s")
-    print('prfill', prefill_bucket_size_to_ms)
-    print('decode step', system_time_per_decode_token_ms)
+  print(
+      f"Idealized out tokens {output_tokens} in {idealized_overall_time:.2f} seconds, for {output_tokens/idealized_overall_time:.2f} out tok/s"
+  )
+  print("prfill", prefill_bucket_size_to_ms)
+  print("decode step", system_time_per_decode_token_ms)
--- benchmarks/run_offline.py
+++ benchmarks/run_offline.py
@@ -31,39 +31,45 @@ logging.getLogger().setLevel(logging.ERROR)
 FLAGS = flags.FLAGS
 
 _TOKENIZER_PATH = flags.DEFINE_string(
-    'tokenizer_path',
-    'tokenizer.model',
-    'The tokenizer model path',
+    "tokenizer_path",
+    "tokenizer.model",
+    "The tokenizer model path",
     required=False,
 )
 _CKPT_PATH = flags.DEFINE_string(
-    'checkpoint_path', None, 'Directory for .pth checkpoints', required=False
+    "checkpoint_path", None, "Directory for .pth checkpoints", required=False
 )
 _BF16_ENABLE = flags.DEFINE_bool(
-    'bf16_enable', False, 'Whether to enable bf16', required=False
+    "bf16_enable", False, "Whether to enable bf16", required=False
 )
 _CONTEXT_LENGTH = flags.DEFINE_integer(
-    'context_length', 1024, 'The context length', required=False
+    "context_length", 1024, "The context length", required=False
 )
 _BATCH_SIZE = flags.DEFINE_integer(
-    'batch_size', 32, 'The batch size', required=False
+    "batch_size", 32, "The batch size", required=False
 )
-_PROFILING_OUTPUT =flags.DEFINE_string(
-    'profiling_output',
-    '',
-    'The profiling output',
+_PROFILING_OUTPUT = flags.DEFINE_string(
+    "profiling_output",
+    "",
+    "The profiling output",
     required=False,
 )
 
-_SIZE = flags.DEFINE_string('size', 'tiny', 'size of model')
+_SIZE = flags.DEFINE_string("size", "tiny", "size of model")
 
-_QUANTIZE_WEIGHTS = flags.DEFINE_bool('quantize_weights', False, 'weight quantization')
-_QUANTIZE_KV_CACHE = flags.DEFINE_bool('quantize_kv_cache', False, 'kv_cache_quantize')
-_MAX_CACHE_LENGTH = flags.DEFINE_integer('max_cache_length', 1024, 'kv_cache_quantize')
+_QUANTIZE_WEIGHTS = flags.DEFINE_bool(
+    "quantize_weights", False, "weight quantization"
+)
+_QUANTIZE_KV_CACHE = flags.DEFINE_bool(
+    "quantize_kv_cache", False, "kv_cache_quantize"
+)
+_MAX_CACHE_LENGTH = flags.DEFINE_integer(
+    "max_cache_length", 1024, "kv_cache_quantize"
+)
 
 
 def create_engine():
-  jax.config.update('jax_default_prng_impl', 'unsafe_rbg')
+  jax.config.update("jax_default_prng_impl", "unsafe_rbg")
   os.environ["TF_CPP_MIN_LOG_LEVEL"] = "0"
 
   max_prefill_predict_length = 1024
@@ -71,31 +77,31 @@ def create_engine():
   devices = jax.devices()
   start = time.perf_counter()
   engine = je.create_pytorch_engine(
-        devices=devices,
-        tokenizer_path=_TOKENIZER_PATH.value,
-        ckpt_path=_CKPT_PATH.value,
-        bf16_enable=True,
-        param_size=_SIZE.value,
-        context_length=_CONTEXT_LENGTH.value,
-        batch_size=_BATCH_SIZE.value,
-        quantize_weights=_QUANTIZE_WEIGHTS.value,
-        quantize_kv=_QUANTIZE_KV_CACHE.value,
-        max_cache_length = _MAX_CACHE_LENGTH.value,
+      devices=devices,
+      tokenizer_path=_TOKENIZER_PATH.value,
+      ckpt_path=_CKPT_PATH.value,
+      bf16_enable=True,
+      param_size=_SIZE.value,
+      context_length=_CONTEXT_LENGTH.value,
+      batch_size=_BATCH_SIZE.value,
+      quantize_weights=_QUANTIZE_WEIGHTS.value,
+      quantize_kv=_QUANTIZE_KV_CACHE.value,
+      max_cache_length=_MAX_CACHE_LENGTH.value,
   )
 
-  print('Initialize engine', time.perf_counter() - start)
+  print("Initialize engine", time.perf_counter() - start)
   return engine
 
 
 def run_prefill_time(engine, params, decode_state, seqlen):
   metadata = engine.get_tokenizer()
-  vocab = token_utils.load_vocab(
-    metadata.path, metadata.extra_ids)
+  vocab = token_utils.load_vocab(metadata.path, metadata.extra_ids)
   tokenizer = vocab.tokenizer
 
-  text = 'This is a beautiful day'
+  text = "This is a beautiful day"
   tokens, true_length = token_utils.tokenize_and_pad(
-    text, vocab, is_bos=True, prefill_lengths=[seqlen])
+      text, vocab, is_bos=True, prefill_lengths=[seqlen]
+  )
 
   for _ in range(3):
     prefill_result = engine.prefill(
@@ -119,7 +125,16 @@ def run_prefill_time(engine, params, decode_state, seqlen):
   return (end - start) / nums, decode_state
 
 
-MAXTEXT_PREFILL = {16:0, 32: 0, 64 : 14.02, 128:18.29, 256:23.59, 512:35.28, 1024: 60.28}
+MAXTEXT_PREFILL = {
+    16: 0,
+    32: 0,
+    64: 14.02,
+    128: 18.29,
+    256: 23.59,
+    512: 35.28,
+    1024: 60.28,
+}
+
 
 def main(argv):
 
@@ -127,7 +142,7 @@ def main(argv):
 
   start = time.perf_counter()
   params = engine.load_params()
-  print('Load params ', time.perf_counter() - start)
+  print("Load params ", time.perf_counter() - start)
 
   prefill_times = {}
   slot = jnp.int32(1)
@@ -136,46 +151,44 @@ def main(argv):
     jax.profiler.start_trace(_PROFILING_OUTPUT.value)
   decode_state = engine.init_decode_state()
   for batch in MAXTEXT_PREFILL.keys():
-    runtime, decode_state = run_prefill_time(engine, params, decode_state, batch)
+    runtime, decode_state = run_prefill_time(
+        engine, params, decode_state, batch
+    )
     prefill_times[batch] = runtime
 
   sampled_tokens_list = []
 
-  for i in range(3): # warm up
-    decode_state, sampled_tokens = engine.generate(
-      params, decode_state
-    )
+  for i in range(3):  # warm up
+    decode_state, sampled_tokens = engine.generate(params, decode_state)
     sampled_tokens_list.append(sampled_tokens)
 
-  print('======= decode starting ===')
+  print("======= decode starting ===")
   dec_times = []
   for i in range(10):
     start = time.perf_counter()
-    decode_state, sampled_tokens = engine.generate(
-      params, decode_state
-    )
+    decode_state, sampled_tokens = engine.generate(params, decode_state)
     jax.block_until_ready(decode_state)
     sampled_tokens_list.append(sampled_tokens)
     end = time.perf_counter()
     dec_times.append(end - start)
-    print(i, 'decode time', (end - start))
-
+    print(i, "decode time", (end - start))
 
   if _PROFILING_OUTPUT.value:
     jax.profiler.stop_trace()
 
-  print('prefill ', prefill_times)
-  print('decode', sum(dec_times) / 10 )
+  print("prefill ", prefill_times)
+  print("decode", sum(dec_times) / 10)
 
-  prefill_times_ms = {k: v*1000 for k, v in prefill_times.items()}
+  prefill_times_ms = {k: v * 1000 for k, v in prefill_times.items()}
   decode_time_ms = sum(dec_times) * 1000 / 10 / _BATCH_SIZE.value
 
   import analyze_sharegpt
-  analyze_sharegpt.do_simulation(prefill_times_ms, decode_time_ms)
 
+  analyze_sharegpt.do_simulation(prefill_times_ms, decode_time_ms)
 
 
 if __name__ == "__main__":
   import os
+
   os.environ["TF_CPP_MIN_LOG_LEVEL"] = "0"
   app.run(main)
