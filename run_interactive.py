55c6c7fefdd8614c09be931af1ee201bfe39dfed
Siyuan Liu <lsiyuan@google.com>
2024-05-18 00:20:10 +0000

    update run_interactive

--- run_interactive.py
+++ run_interactive.py
@@ -26,11 +26,9 @@ from jetstream_pt import engine as je
 from jetstream_pt.config import (
     FLAGS,
     create_engine_from_config_flags,
-    define_common_flags,
     define_profiling_flags,
 )
 
-define_common_flags()
 define_profiling_flags()
 
 
