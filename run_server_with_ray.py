5be8e7c40d461a5411a63f38e9ad549d80d3ab1e
FanhaiLu1 <fanhai@google.com>
2024-06-03 17:37:49 +0000

    add jax profiler server

--- run_server_with_ray.py
+++ run_server_with_ray.py
@@ -34,6 +34,9 @@ flags.DEFINE_string(
 flags.DEFINE_integer("prometheus_port", 0, "")
 flags.DEFINE_integer("tpu_chips", 16, "device tpu_chips")
 
+flags.DEFINE_bool("enable_jax_profiler", False, "enable jax profiler")
+flags.DEFINE_integer("jax_profiler_port", 9999, "port of JAX profiler server")
+
 
 def create_engine():
   """create a pytorch engine"""
@@ -53,6 +56,8 @@ def create_engine():
       quantize_kv=FLAGS.quantize_kv_cache,
       max_cache_length=FLAGS.max_cache_length,
       sharding_config=FLAGS.sharding_config,
+      enable_jax_profiler=FLAGS.enable_jax_profiler,
+      jax_profiler_port=FLAGS.jax_profiler_port,
   )
 
   print("Initialize engine", time.perf_counter() - start)
