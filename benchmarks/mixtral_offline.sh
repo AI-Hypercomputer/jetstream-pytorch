0f7e6fe553e92adbe0431b795a8bb3cd6de17e1e
Han Qi <hanq@google.com>
2024-07-24 15:41:10 +0000

    just act quant

--- benchmarks/mixtral_offline.sh
+++ benchmarks/mixtral_offline.sh
@@ -1,11 +1,13 @@
-CACHE_LENGTH=1024
+CACHE_LENGTH=$1
 INPUT_SIZE=512
 OUTPUT_SIZE=1024
-BATCH_SIZE=512
+BATCH_SIZE=$2
 CHECKPOINT_PATH=mlperf/data/mixtral-instruct-quantized/
 
 pushd ..
 python -m benchmarks.run_offline \
+  --internal_mixtral_act_quant_w1=0 \
+  --internal_mixtral_act_quant_w2=0 \
   --model_name=mixtral \
   --batch_size=$BATCH_SIZE \
   --max_cache_length=$CACHE_LENGTH \
@@ -16,5 +18,5 @@ python -m benchmarks.run_offline \
   --quantize_weights=1 \
   --quantize_type=int8_per_channel \
   --quantize_kv_cache=1 \
-  --profiling_output=/mnt/disks/hanq/mixtral-profiles
+  --profiling_output=/tmp/mixtral-profiles
 popd
--- jetstream_pt/third_party/mixtral/model.py
+++ jetstream_pt/third_party/mixtral/model.py
@@ -16,14 +16,23 @@
 from dataclasses import dataclass
 from typing import Optional, List, Any
 
+import jax.numpy as jnp
 import torch
 import torch.nn as nn
 from torch import Tensor
 from torch.nn import functional as F
 from .config import ModelArgs, find_multiple
+from jetstream_pt import quantize, torchjax
 from jetstream_pt.layers import Attention, get_quantized_linear_layer, get_quantized_enbedding_layer
 
 import jax
+
+from absl import flags
+
+flags.DEFINE_bool('internal_mixtral_act_quant_w1', True, '')
+flags.DEFINE_bool('internal_mixtral_act_quant_w2', True, '')
+
+FLAGS = flags.FLAGS
 
 
 class Transformer(nn.Module):
@@ -245,6 +254,30 @@ class Int8ConditionalFeedForward(nn.Module):
       )
     return expert_outs
 
+  def _int_ti_eoi_teo(self, lhs, rhs):
+    # x1 = F.silu(torch.einsum("ti,eoi -> teo", x, self.w1) * self.w1_scaler)
+    result = torchjax.call_jax(
+        jax.lax.dot_general,
+        lhs,
+        rhs,
+        (((1,), (2)), ((), ())),
+        None,
+        jnp.bfloat16.dtype,
+    )
+    return result
+
+  def _int_teo_eio_tei(self, lhs, rhs):
+    #torch.einsum("teo, eio -> tei", (x1 * x3), self.w2) * self.w2_scaler
+    result = torchjax.call_jax(
+        jax.lax.dot_general,
+        lhs,
+        rhs,
+        (((2,), (2,)), ((1, ), (0, ))),
+        None,
+        jnp.bfloat16.dtype,
+    ) # output is (eti) for some reason
+    return result.transpose(0, 1)
+
   def forward_for_long_seq_len(self, x, expert_indices):
     seqlen = x.shape[0]
     num_experts = self.w1.shape[0]
@@ -254,11 +287,25 @@ class Int8ConditionalFeedForward(nn.Module):
     # o = config.imtermediate size
     # i = config.dim
     with jax.named_scope("conditional_ff"):
-      x1 = F.silu(torch.einsum("ti,eoi -> teo", x, self.w1) * self.w1_scaler)
-      x3 = torch.einsum("ti, eoi-> teo", x, self.w3) * self.w3_scaler
-      expert_outs = (
+      if FLAGS.internal_mixtral_act_quant_w1:
+        x_int, x_scaler, _ = quantize.quantize_tensor(x, (1,))
+        x_scaler = x_scaler.reshape(seqlen, 1, 1)
+        x1 = F.silu(self._int_ti_eoi_teo(x_int, self.w1) * self.w1_scaler * x_scaler)
+        x3 = self._int_ti_eoi_teo(x_int, self.w3) * self.w3_scaler * x_scaler
+      else:
+        x1 = F.silu(torch.einsum("ti,eoi -> teo", x, self.w1) * self.w1_scaler)
+        x3 = torch.einsum("ti, eoi-> teo", x, self.w3) * self.w3_scaler
+      
+      if FLAGS.internal_mixtral_act_quant_w2:
+        x1x3_int, x1x3_scaler, _ = quantize.quantize_tensor(x1 * x3, (2, ))
+        x1x3_scaler = x1x3_scaler.reshape(seqlen, num_experts, 1)
+        expert_outs = (
+            self._int_teo_eio_tei(x1x3_int, self.w2) * self.w2_scaler * x1x3_scaler
+        )
+      else:
+        expert_outs = (
           torch.einsum("teo, eio -> tei", (x1 * x3), self.w2) * self.w2_scaler
-      )
+        )
       # e = 8; need to reduce to 2
       seq_indexes = torch.arange(seqlen).unsqueeze(1)
       return expert_outs[seq_indexes, expert_indices]
