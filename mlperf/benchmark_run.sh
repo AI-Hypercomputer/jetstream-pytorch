9b22689f63d797e19f15a0913ce81594bf75738f
Han Qi <hanq@google.com>
2024-07-26 05:14:27 +0000

    3.5 times output for cache classification

--- mlperf/benchmark_run.sh
+++ mlperf/benchmark_run.sh
@@ -3,13 +3,13 @@ API_URL=0.0.0.0:9000
 USER_CONFIG=$BASEDIR/user.conf
 DATA_DISK_DIR=$BASEDIR/data
 TOTAL_SAMPLE_COUNT=24576
-DATASET_PATH=/mnt/disks/hanq/jetstream-pytorch/mlperf/data/open_orca_gpt4_tokenized_llama.sampled_24576.pkl
+DATASET_PATH=data/open_orca_gpt4_tokenized_llama.sampled_24576.pkl
 
 # HF model id
 #TOKENIZER_PATH="mistralai/Mixtral-8x7B-Instruct-v0.1"
 TOKENIZER_PATH="meta-llama/Llama-2-70b-hf"
 
-LOADGEN_RUN_TYPE=server-performance
+LOADGEN_RUN_TYPE=offline-performance
 OUTPUT_LOG_DIR=${DATA_DISK_DIR}/logs/${OUTPUT_LOG_ID}
 OUTPUT_LOG_ID=${MODEL_NAME}-${DATASET_TYPE}-${LOADGEN_RUN_TYPE}-${LOADGEN_RUN_TIMESTAMP}
 
@@ -18,7 +18,7 @@ mkdir -p ${OUTPUT_LOG_DIR} && cp ../${USER_CONFIG} ${OUTPUT_LOG_DIR}
 python -m main \
 	--accuracy \
 	--api-url ${API_URL} \
-	--scenario Server \
+	--scenario Offline \
 	--is-stream \
 	--input-mode tokenized \
   --output-mode tokenized \
