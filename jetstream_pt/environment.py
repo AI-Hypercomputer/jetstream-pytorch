049531225612b2b668ac9c90d4a61f892123dd5d
Lance Wang <lancewang@google.com>
2024-08-19 21:37:26 +0000

    Fix lint issue.

--- jetstream_pt/environment.py
+++ jetstream_pt/environment.py
@@ -124,7 +124,7 @@ class JetEngineEnvironmentData:
   # The ratio between query heads and kv heads
   n_reps: int = 0
 
-
+# pylint: disable-next=all
 class JetEngineEnvironment:
 # pylint: disable-next=all
   def __init__(self, data: JetEngineEnvironmentData):
