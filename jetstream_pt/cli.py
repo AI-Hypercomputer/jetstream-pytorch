3e46b8eb4bdc7f1bc1f5958eeb892934596773f5
Alvaro Moran <6949769+tengomucho@users.noreply.github.com>
2024-12-13 19:26:39 +0100

    fix: correct error message (#193)
    * fix: correct error message
    
    It was supposed to print the received array but it was not doing it.
    Also, the type is now displayed.
    
    * review: fix format with pyink

--- jetstream_pt/cli.py
+++ jetstream_pt/cli.py
@@ -40,6 +40,7 @@ flags.DEFINE_bool(
 flags.DEFINE_bool("enable_model_warmup", False, "enable model warmup")
 
 
+
 def shard_weights(env, weights, weight_shardings):
   """Shard weights according to weight_shardings"""
   sharded = {}
